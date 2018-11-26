e2ed3cf3904d6c4c7911411abacda430c0fa8d1d
Juo-Ruey Lee <37824551+Randy821216@users.noreply.github.com>
2018-11-26 14:14:06 +0800

    Update jpg2png.py

--- jpg2png.py
+++ jpg2png.py
@@ -16,16 +16,14 @@ last_modified = None
 for filename in filenames:
     modified = get_time(filename)
 
-    #決定流水號，若修改的日期與前一個檔案相同時流水號加 1
+    # Sorting the files with date, and set serial number if the date is same
     if last_modified and last_modified.date() == modified.date():
         num += 1
     else:
         num = 1
 
-    #依據流水號決定檔案
+    # Name:{serial number}.png
     targetname = "{}.png".format(num)
-
-    #改名
     shutil.move(filename, targetname)
 
     last_modified = modified
